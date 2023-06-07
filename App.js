e22bd8d8df78964a7faa9315babb7171001f90e5
suryaa6666 <suryaelidanto@gmail.com>
2023-06-07 13:57:25 +0700

    feat(App.js): add Native Base components to App.js and remove FlatList.js screen feat(package.json): add native-base, react-native-svg, and react-native-safe-area-context dependencies to project

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native";
-import FlatList from "./src/screens/FlatList";
+import {
+  NativeBaseProvider,
+  Text,
+  Box,
+  View,
+  HStack,
+  Stack,
+} from "native-base";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <FlatList />
-      <StatusBar style={{ flex: 1 }} />
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <View backgroundColor={"amber.500"} marginTop={50}>
+        <StatusBar style={{ flex: 1 }} />
+        <Text>NATIVE BASE INTRODUCTION</Text>
+        <Stack direction={"row"} space={10} flexWrap={"wrap"}>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+          <Box borderColor={"primary.300"} borderWidth={3} marginBottom={5}>
+            Halo
+          </Box>
+        </Stack>
+      </View>
+    </NativeBaseProvider>
   );
 }
